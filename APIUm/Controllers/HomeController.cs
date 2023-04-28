9bb900057f38043cedf8655d5c7bc900e5a73967
Gabriel Motta Trevizoli <gabrieltrevizoli@betalearning.com.br>
2023-04-28 12:37:42 -0300

    poc para mostrar diferencas de DI scope

---
+++ APIUm/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using DI___Life_Cycle.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DI___Life_Cycle.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IOperationTransient _transientOperation1;
+        private readonly IOperationTransient _transientOperation2;
+
+        private readonly IOperationScoped _scopedOperation1;
+        private readonly IOperationScoped _scopedOperation2;
+
+        private readonly IOperationSingleton _singletonOperation1;
+        private readonly IOperationSingleton _singletonOperation2;
+
+        public HomeController(
+            IOperationTransient transientOperation1,
+            IOperationTransient transientOperation2,
+            IOperationScoped scopedOperation1,
+            IOperationScoped scopedOperation2,
+            IOperationSingleton singletonOperation1,
+            IOperationSingleton singletonOperation2)
+        {
+            _transientOperation1 = transientOperation1;
+            _transientOperation2 = transientOperation2;
+            _scopedOperation1 = scopedOperation1;
+            _scopedOperation2 = scopedOperation2;
+            _singletonOperation1 = singletonOperation1;
+            _singletonOperation2 = singletonOperation2;
+        }
+
+        [HttpGet]
+        public IActionResult LiefCycleReturn()
+        {
+            return Ok($"Transient1 : {_transientOperation1.OperationId} \n" +
+                      $"Transient2 : {_transientOperation2.OperationId} \n\n" +
+                      $"Scoped1 : {_scopedOperation1.OperationId} \n" +
+                      $"Scoped2 : {_scopedOperation2.OperationId} \n\n" +
+                      $"Singleton1 : {_singletonOperation1.OperationId} \n" +
+                      $"Singleton2 : {_singletonOperation2.OperationId}");
+        }
+    }
+}
